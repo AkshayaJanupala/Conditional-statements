fe0dba90c43e7bd289e82f7d867c84123392fbb3
91998 <akshayajanupala739@gmail.com>
2024-04-23 06:47:21 +0530

    first project

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+ public class Main {
+     public static void main(String[] args) {
+//         System.out.println("Hi my name is Akshaya Reddy");
+//         int age=20;
+//         System.out.println("age ::"+age);
+//         double temperature=78.6;
+//         System.out.println("temperature :: "+temperature);
+//         char isMinor='s';
+//         System.out.println("Is Akshaya Minor ::"+isMinor);
+//         boolean isEligible=true;
+//         System.out.println("Is ram eligible for loan ::"+isEligible);
+
+     }
+}
---
+++ src/scannerDemo.java
@@ -0,0 +1,6 @@
+public class scannerDemo {
+
+    public static void main(String[] args) {
+
+    }
+}
